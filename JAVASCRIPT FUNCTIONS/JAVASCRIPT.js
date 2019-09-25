9ee5f341b3be0075a013f65391ba797994c5301c
Zeus <rhlmalik17@gmail.com>
2019-09-25 12:20:00 +0530

    25 September 2019

---
+++ JAVASCRIPT FUNCTIONS/JAVASCRIPT.js
@@ -0,0 +1,15 @@
+// function demo(x)
+// {
+//     console.log(x);
+//     console.log(y);
+// }
+function demo() {
+ console.log("Hello");
+
+}
+function show(x)
+{
+    console.log("show function");
+    x();
+}
+show(demo);
---
+++ JSIntro/index.js
@@ -0,0 +1,10 @@
+/*
+*   Program 1: BASIC ARITHMATIC OPERATIONS
+*   Program 2: Grades:-
+*               %age > 90 - A GRADE
+*               %age > 70 - B GRADE
+*               %age > 60 - C GRADE
+*               %age > 50 - D GRADE
+*               %age < 50 - F GRADE0
+*   Program 3:
+* */
